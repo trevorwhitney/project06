5ef3677c3e001672ca1d87bb3fe8b4c568b2dea5
Trevor Whitney <trevorjwhitney@gmail.com>
2012-03-27 11:34:19 -0600

    added create and new action to users controller and related views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		flash[:notice] = "Registration suceeded."
+  		redirect_to root_url
+  	else
+  		render :action => 'new'
+  	end
   end
 
   def edit
   end
 
   def index
+  	@users = User.all
   end
 end
