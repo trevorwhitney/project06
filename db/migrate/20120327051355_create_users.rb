09a2dd40af8f1d50809dbad9840827954c01fc15
Trevor Whitney <trevorjwhitney@gmail.com>
2012-03-27 11:33:09 -0600

    fixed typo in user model, added username field

--- db/migrate/20120327051355_create_users.rb
+++ db/migrate/20120327051355_create_users.rb
@@ -1,15 +1,15 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :username
       t.string :first_name
       t.string :last_name
       t.string :email
 
       #Authlogic Stuff
-      t.string :login
       t.string :crypted_password
       t.string :password_salt
-      t.string :persistance_token
+      t.string :persistence_token
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,13 +14,13 @@
 ActiveRecord::Schema.define(:version => 20120327051355) do
 
   create_table "users", :force => true do |t|
+    t.string   "username"
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.string   "login"
     t.string   "crypted_password"
     t.string   "password_salt"
-    t.string   "persistance_token"
+    t.string   "persistence_token"
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
   end
