e6330a2763e0805f27a8e2cb4d5311e28d82a424
Trevor Whitney <trevorjwhitney@gmail.com>
2012-03-27 14:55:43 -0600

    adjusted flash message type, and created helper for displaying them

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,21 @@ module ApplicationHelper
 		link_to "Register", new_user_path
 	end
 
+  def print_flash_messages
+    message = ""
+    if flash[:notice]
+      message += "p(notice). "
+      message += flash[:notice]
+    end
+    if flash[:success]
+      message += "p(success). "
+      message += flash[:success]
+    end
+    if flash[:error]
+      message += "p(error). "
+      message += flash[:error]
+    end
+    raw RedCloth.new(message).to_html
+  end
+
 end
