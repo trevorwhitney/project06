5492c13315463ae67d47c2084244709a8b8593a3
Trevor Whitney <trevorjwhitney@gmail.com>
2012-03-29 16:42:27 -0600

    fixed error in seed data, needed both password and password_confirmation fields

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,18 +14,20 @@ admin = User.new do |u|
   u.first_name = "Joe"
   u.last_name = "Admin"
   u.password = "password"
+  u.password_confirmation = "password"
   u.username = "administrator"
   u.email = "JoeAdmin@gamez.com"
-  u.roles = [1]
+  u.role_ids = [1]
 end
 
 member = User.new do |u|
   u.first_name = "Joe"
   u.last_name = "Member"
   u.password = "password"
+  u.password_confirmation = "password"
   u.username = "member"
   u.email = "JoeMember@gamez.com"
-  u.roles = [2]
+  u.role_ids = [2]
 end
 
 admin.save
