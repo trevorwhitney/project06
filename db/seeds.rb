ac26ad80027e63d573e532075c62085f46949747
Trevor Whitney <trevorjwhitney@gmail.com>
2012-03-29 14:48:53 -0600

    added roles to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+%w{Administrator Member}.each do |role|
+  Role.create(:name => role)
+end
+
