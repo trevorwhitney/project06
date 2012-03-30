1dfb2d20a1092c8df97e3c93f6a5a7b7730dd055
Trevor Whitney <trevorjwhitney@gmail.com>
2012-03-29 22:46:58 -0600

    disabled username field for non-admins

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  def username_text_field(form)
+    if @current_user.is_admin?
+      form.text_field :username
+    else
+      form.text_field :username, :disabled => true
+    end
+  end
+
 end
