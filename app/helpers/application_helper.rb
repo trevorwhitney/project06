6a0e3d03caa094009b95f2bcd37e9798a36afc62
Trevor Whitney <trevorjwhitney@gmail.com>
2012-03-29 21:59:50 -0600

    added admin nav partial and logic to display it when necessary

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,28 @@
 module ApplicationHelper
 	
 	def user_actions
-    if current_user
-      user_actions = "#{link_to "Edit profile", edit_user_path(current_user)} | " +
-        "#{link_to "Logout", logout_path}"
+    if @current_user
+      user_actions = "Logged in as " +
+        "#{link_to @current_user.full_name, edit_user_path(@current_user)}. "
+      if @current_user.is_admin?
+        user_actions += link_to "Admin", "/admin"
+      else
+        user_actions += link_to "My Games", "/members/games"
+      end
+      user_actions += " #{link_to "Logout", logout_path}"
     else
       user_actions = "#{link_to "Register", new_user_path} | " + 
         "#{link_to "Login", login_path}"
     end
+
     raw user_actions
 	end
+
+  def admin_nav
+    if @current_user.is_admin?
+      render "admin/admin_nav"
+    end
+  end
 
   def print_flash_messages
     message = ""
