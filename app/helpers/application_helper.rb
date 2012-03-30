8e5f03c675782d73e40843a6bb819b1d987359c2
Trevor Whitney <trevorjwhitney@gmail.com>
2012-03-30 16:09:22 -0600

    updated views and helpers for the new namespaces

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,15 @@
 module ApplicationHelper
 
   def currently_logged_in_user
-    link_to @current_user.full_name, edit_admin_user_path(@current_user)
+    if current_user.is_admin?
+      link_to current_user.full_name, edit_admin_user_path(current_user)
+    else
+      link_to current_user.full_name, members_profile_path
+    end
   end
 
   def current_role_link
-    if @current_user.is_admin?
+    if current_user.is_admin?
       link_to "Admin", "/admin"
     else
       link_to "My Games", "/members/games"
@@ -13,7 +17,7 @@ module ApplicationHelper
   end
 
   def admin_nav
-    if @current_user.is_admin?
+    if current_user.is_admin?
       render "admin/admin_nav"
     end
   end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,15 @@
 module UsersHelper
 
   def username_text_field(form)
-    if @current_user.is_admin?
+    if current_user.is_admin?
       form.text_field :username
     else
       form.text_field :username, :disabled => true
     end
+  end
+
+  def user_roles
+    current_user.role_symbols.compact.join(", ").to_s.humanize
   end
 
 end
